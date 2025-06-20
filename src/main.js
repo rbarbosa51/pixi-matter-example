c7e8b4332dbdcc1e6ebd7cc38554522ec8a0a7a2
Rafael Barbosa <rafael.barbosa51@gmail.com>
2025-06-20 09:16:07 -0400

    Mouse Usage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import {Engine, Bodies, Composite} from 'matter-js'
+import {Engine, Bodies, Composite, Mouse, MouseConstraint} from 'matter-js'
 import { Application, Assets, Container, Sprite } from 'pixi.js';
-import metalTexture from './metal_texture.jpeg'
-import bricks from './Bricks2.png'
+import metalTextureImg from './metal_texture.jpeg'
+import bricksTextureImg from './Bricks2.png'
 
 (async () =>
 {
@@ -16,42 +16,46 @@ import bricks from './Bricks2.png'
     document.body.appendChild(app.canvas);
 
     // Create and add a container to the stage
-    const container = new Container();
+    const boxContainer = new Container();
     const containerGround = new Container()
-    app.stage.addChild(container);
+    app.stage.addChild(boxContainer);
     app.stage.addChild(containerGround)
     // Load the bunny texture
     
-    const texture = await Assets.load(metalTexture);
-    const texture2 = await Assets.load(bricks)
+    const metalTexture = await Assets.load(metalTextureImg);
+    const floorTexture = await Assets.load(bricksTextureImg)
     
-    //128 x 128
-    const bunny = new Sprite(texture);
-    container.addChild(bunny);
+    const boxSprite = new Sprite(metalTexture);
+    boxContainer.addChild(boxSprite);
     // Move the container to the center
-    container.x = app.screen.width / 2;
-    container.y = app.screen.height / 2;
+    boxContainer.x = app.screen.width / 2;
+    boxContainer.y = app.screen.height / 2;
     // Center the bunny sprites in local container coordinates
-    container.pivot.x = container.width / 2;
-    container.pivot.y = container.height / 2;
+    boxContainer.pivot.x = boxContainer.width / 2;
+    boxContainer.pivot.y = boxContainer.height / 2;
 
-    const groundSprite = new Sprite(texture2)
+    const groundSprite = new Sprite(floorTexture)
     containerGround.addChild(groundSprite)
     containerGround.x = 0
     containerGround.y = window.innerHeight - 50
 
+    
     //Matter.JS
     const engine = Engine.create()
-    // const world = engine.world
-    const boxBunny = Bodies.rectangle(container.x, container.y,128,128)
-    // const ground = Bodies.rectangle(0,window.innerHeight - 50, window.innerWidth,50, {isStatic: true, })
+    const boxBody = Bodies.rectangle(boxContainer.x, boxContainer.y,128,128)
+    const groundBody = Bodies.rectangle(400,window.innerHeight - 30, window.innerWidth,50, {isStatic: true, })
+    
+    const mouse = Mouse.create(app.canvas)
+    const mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      // constraint: {
+      //   render: {
+      //     visible: false,
+      //   }
+      // }
+    })
 
-
-    const ground = Bodies.rectangle(400,window.innerHeight - 30, window.innerWidth,50, {isStatic: true, })
-    Composite.add(engine.world, [boxBunny, ground])
-    // Composite.add(engine.world, [boxBunny])
-
-
+    Composite.add(engine.world, [boxBody, groundBody, mouseConstraint])
 
     // Listen for animate update
     app.ticker.add((time) =>
@@ -60,6 +64,6 @@ import bricks from './Bricks2.png'
         // * use delta to create frame-independent transform *
         // container.rotation -= 0.01 * time.deltaTime;
         Engine.update(engine)
-        container.position.set(boxBunny.position.x, boxBunny.position.y)
+        boxContainer.position.set(boxBody.position.x, boxBody.position.y)
     });
 })();
